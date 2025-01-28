2ea79d5f3c3de438ef43ad22bd66b7fe32ede57b
IagoFP <133005241+IagoFP2004@users.noreply.github.com>
2025-01-28 12:23:02 +0100

    Primer commit

---
+++ app/Controllers/ProductoController.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace Com\Daw2\Controllers;
+
+use Com\Daw2\Core\BaseController;
+
+class ProductoController extends BaseController
+{
+    public function showView()
+    {
+
+    }
+}
